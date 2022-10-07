8e1f83db2693339bca4b8af956241c3b022b91ae
excalibur03cc <garyun.kong@gmail.com>
2022-10-07 16:35:56 +0000

    new updates again

--- test/test-server.js
+++ test/test-server.js
@@ -30,7 +30,7 @@ describe('GET /', function () {
         if (err) {
           return done(err);
         }
-        chai.assert.isTrue(res.text.includes("<h1>Welcomedsadsadsadas</h1>"));
+        chai.assert.isTrue(res.text.includes("<h1>blah blah</h1>"));
         return done();
       });
 
